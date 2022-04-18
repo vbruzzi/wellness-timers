01c081ce08c8d68c99f669e43de1f0d7f330b1b0
vbruzzi <vitor.wcb@gmail.com>
2022-04-17 21:00:00 -0400

    Added tailwind config files

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <div className="grid place-items-center gap-4">
+      <h1 className="text-4xl font-bold">Wellness Timers</h1>
+
+      <div>d2</div>
+    </div>
+  );
+}
+
+// Put this in an info tooltip at the bottom
+{
+  /* <p className="max-w-[50%] text-sm">
+For anybody that spends large amounts of time in front of screens, there
+are many benefits to adherring to the{" "}
+<a href="https://www.healthline.com/health/eye-health/20-20-20-rule">
+  20-20-20 rule
+</a>{" "}
+for screen time and to stretch your hands every hour. The timer will go
+off once every 20 minutes and will give you a 25 second break, followed
+by another alarm indicating the break is over. Every 3 beeps (roughly 1
+hour), there will be another beep indicating it is time to stretch. This
+break is a bit longer, at 2 minutes.
+</p> */
+}
